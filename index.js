bc814a64964a8c6b59a8aa0ba28e4d52c766343d
atharvaru <160043531+atharvaru@users.noreply.github.com>
2026-04-17 13:22:56 -0400

    Add initial HTML, CSS, and JavaScript files

---
+++ index.js
@@ -0,0 +1,7 @@
+const ctaButton = document.getElementById("cta-button");
+
+if (ctaButton) {
+    ctaButton.addEventListener("click", () => {
+        alert("You're ready to customize this template.");
+    });
+}
